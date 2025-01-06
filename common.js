b36b9a0eeef4b5782447561e29f381c0bc525e4f
zoiebackman <zoiebackman@icloud.com>
2025-01-06 12:39:55 +0100

    Add files via upload

--- common.js
+++ common.js
@@ -1,10 +1,13 @@
-let inputDOM = document.getElementById("createNumbers");
+let inputCreateNumbers = document.getElementById("createNumbers");
 let numbersBox = document.querySelector(".numbersBox")
 let createButton = document.getElementById("createButton");
-let newRandomNumber
+let newRandomNumber = document.getElementById("newNumber")
+let newNumberBox = document.querySelector(".newNumberBox")
+let newNumberBoxText = document.querySelector(".newNumberBoxText")
+let removeButton = document.getElementById("removeButton")
 
 createButton.addEventListener("click", function (eventObjekt) {
-    let number = inputDOM.value;
+    let number = inputCreateNumbers.value;
 
     numbersBox.innerHTML = "";
 
@@ -15,5 +18,43 @@ createButton.addEventListener("click", function (eventObjekt) {
 
         numbersBox.appendChild(cell)
     }
+
+})
+
+newRandomNumber.addEventListener("click", function () {
+    let cells = document.querySelectorAll(".numbersCell")
+
+    let randomIndex = Math.floor(Math.random() * cells.length);
+    let selectedNumber = cells[randomIndex].textContent;
+    let selectedCell = cells[randomIndex]
+    console.log(selectedNumber);
+    newNumberBox.textContent = selectedNumber;
+
+    for (let cell of cells) {
+        if (cell.textContent === selectedNumber) {
+            cell.classList.add("randomNumber");
+        }
+    }
+})
+
+removeButton.addEventListener("click", function () {
+    let cells = document.querySelectorAll(".numbersCell");
+    let selectedNumber = newNumberBox.textContent;
+    console.log(cells)
+
+    let count = 0;
+
+    for (let cell of cells) {
+        if (selectedNumber == cell.textContent) {
+            count++
+            cell.classList.add("numbersCellRemove")
+            cell.textContent = "x"
+            newNumberBoxText.textContent = `${selectedNumber} removed ${count} times`
+        }
+        if (selectedNumber == cell) {
+            console.log("hej")
+        }
+    }
+
 
 })
